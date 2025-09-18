2e423cc8a8ee00ec506d4085b3d0f6109b7b9922
Ariel <arielmachador3@gmail.com>
2025-09-18 20:08:46 -0300

    adicionando lib

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Config/app.dart';
-import 'package:flutter_application_1/Services/databaseService.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await DatabaseService.instance.database;
-  runApp(const App());
-}
